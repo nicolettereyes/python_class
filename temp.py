9185976c4323510e7eda41148244963dea86a395
nicolettereyes <nereyes2005@gmail.com>
2019-07-20 12:20:33 -0700

    add loop

--- temp.py
+++ temp.py
@@ -18,19 +18,29 @@ def calculate(first_number, second_number, operater):
 
 #print(calculate(1,2,'+'))
 
-first_number = input("What is the first number? ") 
-first_number = int(first_number)
 
-second_number = input("What is the second number? ")
-second_number = int(second_number)
+more_input = True
+while more_input:
+  first_number = input("What is the first number? ") 
+  first_number = int(first_number)
+  second_number = input("What is the second number? ")
+  second_number = int(second_number)
 
-operater = input("What operation do you want me to do (+,-,*, /)? ")
-operater = operater.strip()
+  operater = input("What operation do you want me to do (+,-,*, /)? ")
+  operater = operater.strip()
   
-result = calculate(first_number, second_number, operater)
+  result = calculate(first_number, second_number, operater)
 
-if result == OUT_OF_BOUNDS:  
-  print("you need a valid operater (+,-, *, /)")
-else:
-  print (result)
+  if result == OUT_OF_BOUNDS:  
+    print("you need a valid operater (+,-, *, /)")
+  else:
+    print (result)
 
+  if input("Another calculation (y/n)? ") == "y":
+    more_input = True
+  else:
+    more_input = False
+
+#more_input = True(True if input(Another calculation (y/n)?)== 'y' else False
+
+print ("Bye now")
